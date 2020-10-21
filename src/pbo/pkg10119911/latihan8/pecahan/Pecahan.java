60b491ac381e3bbda79d89e590ff47354fd072b4
gare15 <72866876+gare15@users.noreply.github.com>
2020-10-21 08:45:25 +0700

    Pecahan

---
+++ src/pbo/pkg10119911/latihan8/pecahan/Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10119911.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mochamad Tegar Dwi S
+ * KELAS    : IF10K
+ * NIM      : 10119911 
+ * Deskripsi Program : Pecahan
+ */
+public class Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
